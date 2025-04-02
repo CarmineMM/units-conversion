d7154147a94bad7ebf621278daa22c375375958f
Carmine Maggio <carminemaggiom@gmail.com>
2025-04-02 11:10:57 -0400

    feat: new methods in length units conversion

--- src/Conversion/LengthUnitsConversion.php
+++ src/Conversion/LengthUnitsConversion.php
@@ -96,4 +96,278 @@ class LengthUnitsConversion extends BaseConversion
             'known'  => ['nautical-miles', 'nautical-mile', 'milla-nautica', 'millas-nauticas'],
         ],
     ];
+
+    /**
+     * Create a new LengthUnitsConversion instance from micrometers
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromMicrometers(float $value): self
+    {
+        return new self($value, 'μm');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from millimeters
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromMillimeters(float $value): self
+    {
+        return new self($value, 'mm');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from centimeters
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromCentimeters(float $value): self
+    {
+        return new self($value, 'cm');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from inches
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromInches(float $value): self
+    {
+        return new self($value, 'in');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from decimeters
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromDecimeters(float $value): self
+    {
+        return new self($value, 'dm');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from feet
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromFeet(float $value): self
+    {
+        return new self($value, 'ft');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from yards
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromYards(float $value): self
+    {
+        return new self($value, 'yd');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from meters
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromMeters(float $value): self
+    {
+        return new self($value, 'm');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from decameters
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromDecameters(float $value): self
+    {
+        return new self($value, 'dkm');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from hectometers
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromHectometers(float $value): self
+    {
+        return new self($value, 'hm');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from kilometers
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromKilometers(float $value): self
+    {
+        return new self($value, 'km');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from miles
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromMiles(float $value): self
+    {
+        return new self($value, 'mi');
+    }
+
+    /**
+     * Create a new LengthUnitsConversion instance from nautical miles
+     * 
+     * @param float $value
+     * @return self
+     */
+    public static function fromNauticalMiles(float $value): self
+    {
+        return new self($value, 'nmi');
+    }
+
+    /**
+     * Convert to micrometers
+     * 
+     * @return float
+     */
+    public function toMicrometers(): float
+    {
+        return $this->to('μm');
+    }
+
+
+    /**
+     * Convert to millimeters
+     * 
+     * @return float
+     */
+    public function toMillimeters(): float
+    {
+        return $this->to('mm');
+    }
+
+    /**
+     * Convert to centimeters
+     * 
+     * @return float
+     */
+    public function toCentimeters(): float
+    {
+        return $this->to('cm');
+    }
+
+    /**
+     * Convert to meters
+     * 
+     * @return float
+     */
+    public function toMeters(): float
+    {
+        return $this->to('m');
+    }
+
+    /**
+     * Convert to kilometers
+     * 
+     * @return float
+     */
+    public function toKilometers(): float
+    {
+        return $this->to('km');
+    }
+
+    /**
+     * Convert to inches
+     * 
+     * @return float
+     */
+    public function toInches(): float
+    {
+        return $this->to('in');
+    }
+
+    /**
+     * Convert to feet
+     * 
+     * @return float
+     */
+    public function toFeet(): float
+    {
+        return $this->to('ft');
+    }
+
+    /**
+     * Convert to yards
+     * 
+     * @return float
+     */
+    public function toYards(): float
+    {
+        return $this->to('yd');
+    }
+
+    /**
+     * Convert to decimeters
+     * 
+     * @return float
+     */
+    public function toDecimeters(): float
+    {
+        return $this->to('dm');
+    }
+
+    /**
+     * Convert to decameters
+     * 
+     * @return float
+     */
+    public function toDecameters(): float
+    {
+        return $this->to('dkm');
+    }
+
+    /**
+     * Convert to hectometers
+     * 
+     * @return float
+     */
+    public function toHectometers(): float
+    {
+        return $this->to('hm');
+    }
+
+    /**
+     * Convert to miles
+     * 
+     * @return float
+     */
+    public function toMiles(): float
+    {
+        return $this->to('mi');
+    }
+
+    /**
+     * Convert to nautical miles
+     * 
+     * @return float
+     */
+    public function toNauticalMiles(): float
+    {
+        return $this->to('nmi');
+    }
 }
